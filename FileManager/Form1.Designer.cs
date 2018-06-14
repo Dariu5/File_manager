c2d2bdcaa39266aa9baa8559b1de7b3a89fd743a
Darius Vainilavičius <radiocool@gmail.com>
2018-06-14 21:51:35 +0300

    First

--- FileManager/Form1.Designer.cs
+++ FileManager/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxlog = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxpath = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxlog
+            // 
+            this.textBoxlog.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBoxlog.Location = new System.Drawing.Point(0, 252);
+            this.textBoxlog.Multiline = true;
+            this.textBoxlog.Name = "textBoxlog";
+            this.textBoxlog.Size = new System.Drawing.Size(800, 198);
+            this.textBoxlog.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxpath);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(468, 137);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxpath
+            // 
+            this.textBoxpath.Location = new System.Drawing.Point(139, 97);
+            this.textBoxpath.Name = "textBoxpath";
+            this.textBoxpath.Size = new System.Drawing.Size(203, 20);
+            this.textBoxpath.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxlog);
+            this.Name = "Form1";
+            this.Text = "File manager";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxlog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxpath;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- FileManager/Form1.cs
+++ FileManager/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media.Imaging;
 
 namespace FileManager
 {
@@ -15,6 +17,75 @@ namespace FileManager
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            FolderBrowserDialog path = new FolderBrowserDialog();
+
+            if (path.ShowDialog() == DialogResult.OK)
+
+            {
+                textBoxpath.Text = path.SelectedPath.ToString();
+
+            }
+
+            foreach (var file in Directory.EnumerateFiles(path.SelectedPath))
+            {
+
+                BitmapMetadata md = null;
+                FileInfo fileInfo = new FileInfo(file);
+                textBoxlog.AppendText(file.ToString() + " ");
+
+                try
+                {
+                    FileStream fs = new FileStream(fileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.Read);
+                    BitmapSource img = BitmapFrame.Create(fs);
+                    md = (BitmapMetadata)img.Metadata;
+
+                    
+
+
+                    textBoxlog.AppendText(md.DateTaken);
+
+                    DateTimeConverter converter = new DateTimeConverter();
+
+                    var date = converter.ConvertFrom(md.DateTaken.ToString());
+
+                    var data_string = date.ToString().Split(' ');
+
+                     
+                    DirectoryInfo di = Directory.CreateDirectory(path.SelectedPath.ToString() + "/" + data_string[0]);
+
+
+                    fs.Close();
+                    System.IO.File.Move(file.ToString(), di.FullName.ToString() + "/" + fileInfo.Name);
+
+                }
+
+                
+                catch 
+
+                {
+                    textBoxlog.AppendText("SKIPPED");
+
+
+
+                }
+
+               
+                textBoxlog.AppendText(Environment.NewLine);                         
+
+                
+
+            }
         }
     }
 }
